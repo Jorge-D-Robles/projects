232eaed51a331e1b102f07f592bff6503b5bde91
Jorge Robles <robles8925@gmail.com>
2022-10-30 20:10:03 -0400

    started schedule function

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,10 +1,12 @@
-from datetime import date
+from datetime import date, timedelta
 import datetime as dt
+import calendar
 import csv
 import sys
 
 
 def main():
+    make_schedule()
     print("Welcome to the musical lesson schedule builder. To start, please enter a starting date.")
     start_date = get_date()
     print(f"Your start date is {start_date}.")
@@ -24,6 +26,42 @@ def main():
     for i in range(len(days_off)):
         print(days_off[i])
     agree()
+
+
+def make_schedule():
+    current_date = get_date()
+    end_date = current_date + timedelta(days=30)
+    print(end_date)
+    day_cycle = 1
+    grps = 'ABCDEFGHIJKLMAOPQRST'
+    day1_periods = [2, 6, 8, 9]
+    day2_periods = [2, 3, 8, 9]
+    schedule = {
+        # date: {'period': period, 'group': group}
+
+    }
+    while end_date != current_date:
+        date = current_date
+        print(date)
+        date = {}
+        if day_cycle == 1:
+            for period in day1_periods:
+                for group in grps:
+                    print(period)
+                    date["period"] = day1_periods[period]
+                    date["group"] = group
+                    break
+                break
+            print(date)
+        elif day_cycle == 2:
+            for periods in day2_periods:
+                ...
+        schedule[date] = date
+        current_date = current_date + timedelta(days=1)
+
+    print(schedule)
+    return schedule
+
 
 def get_date():
     while True:
@@ -56,7 +94,6 @@ def off():
             break
         elif days_prompt.startswith("y"):
             days_off.append(get_date())
-            # Not working, fix after food.
         else:
             continue
     days_off = sorted(days_off)
