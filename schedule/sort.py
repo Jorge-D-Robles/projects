b6096c1ca99f1f58a76e854830bd8842edbfbb88
Jorge Robles <robles8925@gmail.com>
2022-11-01 21:32:30 -0400

    .

--- schedule/sort.py
+++ schedule/sort.py
@@ -1,9 +1,14 @@
 def main():
-    group_a = ['A', 'B', 'C', 'D']
-    group_b = ['E', 'F', 'G', 'H']
-    group_c = ['I', 'J', 'K', 'L']
-    group_d = ['M', 'N', 'O', 'P']
-    group_e = ['Q', 'R', 'S', 'T']
+    all_groups = [['A', 'B', 'C', 'D']
+                  ['E', 'F', 'G', 'H']
+                  ['I', 'J', 'K', 'L']
+                  ['M', 'N', 'O', 'P']
+                  ['Q', 'R', 'S', 'T']]
+
+    # something % 5, 1 is group a, 2 group b, etc up to 5 = groupe
+    # after the group function has been called 4 times, switch to the next subgroup
+    # after 5 subgroups have been called, begin again, but randomize subgroup order
+
     print(group_a)
     for i in range(4):
         rotate_group(group_a)
@@ -11,11 +16,13 @@ def main():
     print(group_a)
 
 
-#push the last item in the list to the front for group_a
+# push the last item in the list to the front for group_a
 
 def rotate_group(group):
     group.insert(0, group.pop())
     return group
+
+
 
 
 if __name__ == '__main__':
