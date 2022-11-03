f22b30e8b79dfae52f2968c605c604f36937b6ed
Jorge <robles8925@gmail.com>
2022-11-02 22:34:10 -0400

    almost done with rotate groups

--- schedule/sort.py
+++ schedule/sort.py
@@ -1,28 +1,73 @@
+import random
+from datetime import date, timedelta
+from tabulate import tabulate
+group_randomizer = 0
+
+group_change = 0
+
+group_index = 0
+
+total_changes = 0
+
+all_groups = [['A', 'B', 'C', 'D'],
+              ['E', 'F', 'G', 'H'],
+              ['I', 'J', 'K', 'L'],
+              ['M', 'N', 'O', 'P'],
+              ['Q', 'R', 'S', 'T']]
+
+
 def main():
-    all_groups = [['A', 'B', 'C', 'D']
-                  ['E', 'F', 'G', 'H']
-                  ['I', 'J', 'K', 'L']
-                  ['M', 'N', 'O', 'P']
-                  ['Q', 'R', 'S', 'T']]
 
-    # something % 5, 1 is group a, 2 group b, etc up to 5 = groupe
-    # after the group function has been called 4 times, switch to the next subgroup
-    # after 5 subgroups have been called, begin again, but randomize subgroup order
+    print(all_groups[group_index])
+    for i in range(20):
+        print(find_group(all_groups), end='')
+    print("")
+    for i in range(8):
+        for i in range(20):
+            print(find_group(all_groups), end='')
+        print(all_groups[group_index])
+        print("")
 
-    print(group_a)
-    for i in range(4):
-        rotate_group(group_a)
-        print(group_a)
-    print(group_a)
-
-
-# push the last item in the list to the front for group_a
+    current_date = date(2022, 11, 16)
 
 def rotate_group(group):
     group.insert(0, group.pop())
     return group
 
 
+def randomize_group(group):
+    random.shuffle(group)
+    return group[0]
+
+
+def find_group(all_groups):
+    global group_randomizer
+    global group_change
+    global group_index
+    global total_changes
+
+    # group_randomizer += 1
+    # if group_randomizer == 21:
+    #     randomize_group(all_groups)
+    #     group_randomizer = 0
+
+    if total_changes < 19:
+        rotated = all_groups[group_index][group_change]
+
+    else:
+        all_groups = rotate_group(all_groups[group_index])
+        rotated = all_groups
+
+    total_changes += 1
+    group_change += 1
+
+    if group_change == 4:
+        group_index += 1
+        group_change = 0
+        if group_index == 5:
+            group_index = 0
+
+    return rotated[0]
 
 
 if __name__ == '__main__':
