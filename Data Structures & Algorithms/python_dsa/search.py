cdaf0686a1951a0877d1c76138fed15709ac99f2
Jorge Robles <robles8925@gmail.com>
2023-09-21 18:34:28 -0400

    binary search

---
+++ Data Structures & Algorithms/python_dsa/search.py
@@ -0,0 +1,15 @@
+from random import randint
+def generate_array(size, num):
+    arr = set()
+    for i in range(size):
+        arr.add(randint(0, size))
+    arr.add(num)
+
+
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main():
